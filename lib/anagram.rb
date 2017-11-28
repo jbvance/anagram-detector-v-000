a2bb3eeef287a199f100c65ebf76eb384f8952a1
Jason Vance <jbvance@gmail.com>
2017-11-28 15:42:40 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :input
+
+  def initialize(input)
+    @input = input
+  end
+
+  def match(words)
+    input_sorted = @input.split("").sort
+    words.each do |word|
+      return true if input_sorted == word.split("").sort
+    end
+  end
+  
+end
